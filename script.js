e078059e369b9187e7366aa93693b74f0f20e4cd
JohnJohn081 <johnmaclebio@gmail.com>
2024-09-07 16:14:02 -0300

    add new currencies

--- script.js
+++ script.js
@@ -13,20 +13,92 @@ document.getElementById('btn').addEventListener('click', function(){
     console.log('Moeda: ' + moeda)
     switch(moeda){
         case "$":
-            conversão(moeda, valor, 5.60)
+            conversão(moeda, valor, 5.60); 
             break;
         case "€":
-            conversão(moeda, valor, 6.19)
+            conversão(moeda, valor, 6.19); 
             break;
         case "£":
-            conversão(moeda, valor, 7.35)
+            conversão(moeda, valor, 7.35); 
             break;
         case "C$":
-            conversão(moeda, valor, 4.11)
+            conversão(moeda, valor, 4.11); 
             break;
         case "MX$":
-            conversão(moeda, valor, 0.28)
+            conversão(moeda, valor, 0.28); 
             break;
+        case "¥":
+            conversão(moeda, valor, 0.052); 
+            break;
+        case "₹":
+            conversão(moeda, valor, 0.075); 
+            break;
+        case "R$":
+            conversão(moeda, valor, 1.00); 
+            break;
+        case "A$":
+            conversão(moeda, valor, 3.90); 
+            break;
+        case "CHF":
+            conversão(moeda, valor, 6.10); 
+            break;
+        case "₽":
+            conversão(moeda, valor, 0.071); 
+            break;
+        case "₺":
+            conversão(moeda, valor, 0.25); 
+            break;
+        case "₩":
+            conversão(moeda, valor, 0.0045); 
+            break;
+        case "₪":
+            conversão(moeda, valor, 1.50); 
+            break;
+        case "R":
+            conversão(moeda, valor, 0.30); 
+            break;
+        case "₫":
+            conversão(moeda, valor, 0.00023); 
+            break;
+        case "kr":
+            conversão(moeda, valor, 0.54); 
+            break;
+        case "S$":
+            conversão(moeda, valor, 4.20); 
+            break;
+        case "HK$":
+            conversão(moeda, valor, 0.72); 
+            break;
+        case "NZ$":
+            conversão(moeda, valor, 3.65); 
+            break;
+        case "₱":
+            conversão(moeda, valor, 0.11); 
+            break;
+        case "Kč":
+            conversão(moeda, valor, 0.26); 
+            break;
+        case "zł":
+            conversão(moeda, valor, 1.38); 
+            break;
+        case "lei":
+            conversão(moeda, valor, 1.23); 
+            break;
+        case "฿":
+            conversão(moeda, valor, 0.16); 
+            break;
+        case "₦":
+            conversão(moeda, valor, 0.013); 
+            break;
+        case "₴":
+            conversão(moeda, valor, 0.15); 
+            break;
+        case "₭":
+            conversão(moeda, valor, 0.00030); 
+            break;
+        default:
+            console.log("Moeda não encontrada");
     }
+    
 
 })
